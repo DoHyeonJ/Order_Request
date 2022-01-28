b170b81f5cf8ac9aa2ce87c9603780c23e3e7c55
dohyeonj <jdh7693@naver.com>
2022-01-28 20:47:19 +0900

    first commit

---
+++ src/main/java/com/ServletInitializer.java
@@ -0,0 +1,14 @@
+package com;
+
+import com.OrderRequestApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(OrderRequestApplication.class);
+    }
+
+}
---
+++ src/main/java/intro/IntroController.java
@@ -0,0 +1,14 @@
+package intro;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IntroController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+}
